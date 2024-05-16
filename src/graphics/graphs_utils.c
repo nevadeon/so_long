65f5a077e7d38af37c3d044a56959b5ffbafd694
ndavenne <github@noedavenne.aleeas.com>
2024-05-16 14:36:08 +0200

    added valgrind config files to handle mlx leaks

--- src/graphics/graphs_utils.c
+++ src/graphics/graphs_utils.c
@@ -6,16 +6,15 @@
 /*   By: ndavenne <github@noedavenne.aleeas.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:22:20 by ndavenne          #+#    #+#             */
-/*   Updated: 2024/05/15 19:09:26 by ndavenne         ###   ########.fr       */
+/*   Updated: 2024/05/16 14:33:07 by ndavenne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	free_graphics(t_game_visuals *graphs)
+void	free_graphics(t_game_visuals *gv)
 {
-	mlx_delete_image(graphs->mlx, graphs->exit_bt);
-	mlx_delete_image(graphs->mlx, graphs->start_bt);
-	mlx_delete_image(graphs->mlx, graphs->foreground);
-	mlx_terminate(graphs->mlx);
+	mlx_terminate(gv->mlx);
+	free(gv->select_anim.frames);
+	free(gv->menu_bg.frames);
 }
