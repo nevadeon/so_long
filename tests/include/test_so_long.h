2b2b17b4d7cc739bada23dfecd9388884acb5521
ndavenne <github@noedavenne.aleeas.com>
2025-01-06 13:38:12 +0100

    removed header

---
+++ tests/include/test_so_long.h
@@ -0,0 +1,13 @@
+#ifndef TEST_PUSH_SWAP_H
+# define TEST_PUSH_SWAP_H
+
+# include <assert.h>
+# include <stdio.h>
+# include "so_long.h"
+
+# define ft_arena_alloc malloc
+# define exit(error_code) fprintf(stderr, "exit was called with %d\n", error_code);
+
+/* debug */
+
+#endif
--- tests/test_main.c
+++ tests/test_main.c
@@ -6,15 +6,13 @@
 /*   By: nevadeon <github@glhf.slmail.me>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 12:06:29 by ndavenne          #+#    #+#             */
-/*   Updated: 2025/01/06 12:03:46 by nevadeon         ###   ########.fr       */
+/*   Updated: 2025/01/06 13:37:27 by nevadeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "test_push_swap.h"
+#include "test_so_long.h"
 
 int	main(void)
 {
-	assert(min(1, 2, 3, 4) == 1);
-	assert(min(1, 2, 3, 4) == 0);
 	return (0);
 }
